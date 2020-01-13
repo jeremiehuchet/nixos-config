ef02bdbdffd6eb412119fc07ae4e60a055358729
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2020-01-14 00:11:47 +0100

    home manager setup without git submodule

--- configuration.nix
+++ configuration.nix
@@ -6,19 +6,20 @@
 
 {
   imports = [
-     <nixos-hardware/lenovo/thinkpad/x1-extreme/gen2>
+    <home-manager/nixos>
+    <nixos-hardware/lenovo/thinkpad/x1-extreme/gen2>
     ./hardware-configuration.nix
-    ./home-manager/nixos
   ];
+
+  hardware = {
+    bumblebee.enable = true;
+  };
 
   boot.earlyVconsoleSetup = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 20;
   boot.loader.systemd-boot.consoleMode = "max";
-  boot.blacklistedKernelModules = [
-    "nouveau" # See https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Extreme_(Gen_2)#Graphics
-  ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.cleanTmpDir = true;
   boot.initrd.luks.devices = [ {
@@ -33,7 +34,7 @@
   fileSystems."/nix".options = [ "noatime" "nodiratime" "discard" ];
 
   services.udev.extraRules = ''
-    SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="c6:d1:c1:15:11:f7", NAME="wireless"
+    SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="60:f2:62:15:55:db", NAME="wireless"
     SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="48:2a:e3:6b:11:0f", NAME="unknown"
     SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="00:0e:c6:fe:5a:0c", NAME="aukey-ethernet"
   '';
@@ -72,21 +73,16 @@
     ];
   };
 
-  hardware.enableRedistributableFirmware = true;
   nixpkgs.config.allowUnfree = true;
+
+  environment.pathsToLink = [ "/share/zsh" ];
 
   environment.systemPackages = with pkgs; [
     dfc
-    dunst
-    gitFull
-    google-chrome
+    git
     htop
     lm_sensors
-    mplayer
-    rofi
-    terminator
     tree
-    udiskie
     vim
   ];
 
@@ -96,6 +92,7 @@
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
   programs.light.enable = true;
   programs.vim.defaultEditor = true;
+  programs.zsh.enable = true;
 
   services.openssh.enable = true;
   services.printing.enable = true;
@@ -112,45 +109,21 @@
   services.xserver = {
     enable = true;
     layout = "fr";
-    libinput.enable = true;
     dpi = 192;
-    videoDrivers = [ "intel" "nvidia" ];
-
-    windowManager.default = "i3";
-    windowManager.i3.enable = true;
-    windowManager.i3.package = pkgs.i3-gaps;
+    libinput = {
+      enable = true;
+      accelSpeed = "0.5";
+    };
   };
 
   users.users.jeremie = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "video" "wheel" ];
+    shell = pkgs.zsh;
   };
 
+  home-manager.useUserPackages = true;
   home-manager.users.jeremie = import ./home.nix;
-
-  systemd.user.services."dunst" = {
-    enable = true;
-    description = "dunst for X11 notifications";
-    wantedBy = [ "default.target" ];
-    serviceConfig.Restart = "always";
-    serviceConfig.RestartSec = 2;
-    serviceConfig.ExecStart = "${pkgs.dunst}/bin/dunst";
-  };
-
-  systemd.user.services."udiskie" = {
-    enable = true;
-    description = "udiskie to automount removable media";
-    wantedBy = [ "default.target" ];
-    path = with pkgs; [
-      gnome3.defaultIconTheme
-      gnome3.gnome_themes_standard
-      udiskie
-    ];
-    environment.XDG_DATA_DIRS="${pkgs.gnome3.defaultIconTheme}/share:${pkgs.gnome3.gnome_themes_standard}/share";
-    serviceConfig.Restart = "always";
-    serviceConfig.RestartSec = 2;
-    serviceConfig.ExecStart = "${pkgs.udiskie}/bin/udiskie -a -t -n -F ";
-  };
 
   nix.autoOptimiseStore = true;
   nix.gc.automatic = true;
