6ca7a774ae51b2646fa29f69652e058e9a86f63d
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2020-03-19 17:04:55 +0100

    fixup battery osd alert script

--- custom-pkgs/battery-alert.nix
+++ custom-pkgs/battery-alert.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 pkgs.writeScriptBin "battery-alert" ''
-  #!${pkgs.bash}/bin/bash
+  #!${pkgs.bash}/bin/bash -e
   
   LOG="${pkgs.utillinux}/bin/logger -t $(basename $0)"
   XOSD="${pkgs.xosd}/bin/osd_cat --delay=10 --align=center --shadow=5"
@@ -16,14 +16,12 @@ pkgs.writeScriptBin "battery-alert" ''
   fi
 
   if [ $1 -le 10 ] ; then
-    ALERT_CMD="$XOSD_LARGE --colour=red --pos=bottom --barmode=percentage --percentage=$1 -T \"CRITICAL BATTERY LEVEL\""
+    $XOSD_NORMAL --colour=red --pos=bottom --barmode=percentage --percentage=$1 &
+    echo "CRITICAL BATTERY LEVEL" | $XOSD_LARGE --colour=red --pos=bottom --offset=-500
   elif [ $1 -le 30 ] ; then
-    ALERT_CMD="$XOSD_LARGE --colour=yellow --pos=bottom --barmode=percentage --percentage=$1 -T \"LOW BATTERY\""
+    $XOSD_NORMAL --colour=yellow --pos=bottom --barmode=percentage --percentage=$1
+    echo "LOW BATTERY" | $XOSD_LARGE --colour=yellow --pos=bottom --offset=-500
   else
-    ALERT_CMD="$XOSD_LARGE --colour=green --pos=bottom --barmode=percentage --percentage=$1"
+    $XOSD_NORMAL --colour=green --pos=bottom --barmode=percentage --percentage=$1
   fi
-
-  $LOG "$ALERT_CMD"
-  DISPLAY=:0 $ALERT_CMD
-  exit $?
 ''
