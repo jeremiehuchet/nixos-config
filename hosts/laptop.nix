d086bdea27918f466a0cb0808113f81272ba0e02
Jérémie Huchet <jeremiehuchet@users.noreply.github.com>
2020-03-29 23:33:41 +0200

    ✨ enable virtualbox

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -125,8 +125,8 @@ in {
   };
 
   virtualisation.docker.enable = true;
-  #virtualisation.virtualbox.host.enable = true;
-  #virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
 
   users.users.jeremie = {
     isNormalUser = true;
