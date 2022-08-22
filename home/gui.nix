4cb85a556d5df7ba0c7046d9225c6189b4097ccc
Jeremie Huchet <jeremiehuchet@github.users.noreply>
2022-08-22 09:26:56 +0200

    ♻️ xsession.pointerCursor deprecation

--- home/gui.nix
+++ home/gui.nix
@@ -51,6 +51,12 @@ in {
           "Xft.rgba" = "rgb";
         };
 
+        home.pointerCursor = {
+          package = pkgs.vanilla-dmz;
+          name = "Vanilla-DMZ";
+          size = 64;
+        };
+
         xsession = {
           enable = true;
           initExtra = ''
@@ -63,11 +69,6 @@ in {
             export GDK_DPI_SCALE=0.5
             export QT_AUTO_SCREEN_SCALE_FACTOR=1
           '';
-          pointerCursor = {
-            package = pkgs.vanilla-dmz;
-            name = "Vanilla-DMZ";
-            size = 64;
-          };
 
           windowManager.i3 = {
             enable = true;
