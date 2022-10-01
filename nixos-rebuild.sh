d1355bef2834df2e9bdae60a069586e66364fe4e
Jeremie Huchet <jeremiehuchet@github.users.noreply>
2022-10-01 14:26:46 +0200

    🐛 fix system build script

--- nixos-rebuild.sh
+++ nixos-rebuild.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-NIX_PATH=$(nix eval --raw '(builtins.concatStringsSep ":" (import ./pinned-channels).nixPath)')
+NIX_PATH=$(
+  nix --extra-experimental-features nix-command \
+      eval --impure --expr '(builtins.concatStringsSep ":" (import ./pinned-channels).nixPath)' \
+      | sed 's/^"//g' | sed 's/"$//g'
+)
+echo $NIX_PATH
 export NIX_PATH
 
 nixos-rebuild "$@"
