7b0f5dea991894f0108f3f0457e6b115b189a0f7
Jeremie Huchet <jeremiehuchet@github.users.noreply>
2023-08-18 17:23:27 +0200

    🚧 Home assistant

--- custom-pkgs/default.nix
+++ custom-pkgs/default.nix
@@ -1,17 +1,36 @@
 { pkgs, config, ... }:
 
-let channels = import ../pinned-channels;
+let
+  channels = import ../pinned-channels;
+  unstable = import channels.nixpkgs-unstable {
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "openssl-1.1.1w"
+      ];
+    };
+  };
+  nur = import channels.nur-packages { inherit pkgs; };
 in {
 
   nixpkgs.config.allowUnfree = true;
 
   nixpkgs.config.packageOverrides = pkgs: rec {
-    unstable = import channels.nixpkgs-unstable {
-      # propagates "allowUnfree" config
-      config = config.nixpkgs.config; };
-    nur = import channels.nur-packages { inherit pkgs; };
-
-    pretty-nixos-rebuild = pkgs.callPackage ./pretty-nixos-rebuild.nix { };
-    rofi-translate = pkgs.callPackage ./rofi-translate.nix { };
+    inherit unstable;
+    inherit nur;
   };
+
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        home-assistant = unstable.home-assistant;
+        home-assistant-custom-components = prev.home-assistant-custom-components // {
+          livebox = pkgs.nur.hass-livebox-component;
+          prixcarburant = pkgs.callPackage /home/guest/projects/nur-packages/pkgs/hass-prixcarburant-component {
+            prixCarburantFrClient = pkgs.callPackage /home/guest/projects/nur-packages/pkgs/python-packages/prix-carburant-fr-client { };
+          };
+        };
+      }
+      )
+  ];
 }
