0b5aa1c8be0a03118c03c58d4b738bf0ffb8d6b5
Jeremie Huchet <jeremiehuchet@github.users.noreply>
2025-05-02 22:45:41 +0200

    🐛 Fix dyndns ip not updating

--- hosts/tv/dyndns.nix
+++ hosts/tv/dyndns.nix
@@ -17,6 +17,11 @@ in {
       secrets.dyndns.domain
     ];
     usev4 = "cmdv4, cmdv4=${showIpv4}/bin/ip-addr-show-v4";
-    usev6 = "cmdv6, cmdv6=${showIpv6}/bin/ip-addr-show-v6";
+    # not updateing ipv4 and ipv6 at the same time because it fails
+    # SENDING:  url="https://dns.eu.ovhapis.com/nic/update?system=dyndns&hostname=host.domain.tld&myip=136.101.43.163,a3ce:174b:2cca:2fc3:4d13:a092:05c7:4937&wildcard=ON"
+    # RECEIVE:  HTTP/1.1 400 Bad Request
+    # RECEIVE:  {"class":"Client::BadRequest","message":"{'myip': ['Invalid ip']}"}
+    #usev6 = "cmdv6, cmdv6=${showIpv6}/bin/ip-addr-show-v6";
+    #verbose = true;
   };
 }
